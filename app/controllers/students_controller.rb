094c3f5327d552a2c2d17e65b12bd60c4ef29401
Neil <zooming.in@hotmail.com>
2021-09-12 22:52:16 +1000

    updates

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,11 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id]).to_s
+    puts @student
+
+  end
+
 end
